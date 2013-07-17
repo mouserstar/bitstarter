309e74100b3c094de853d77dee10508901e01d5e
Anne Kramig <mouserstar@gmail.com>
2013-07-17 02:17:22 +0000

    updated web.js again

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 var buffer = new buffer("I'm a string!", "utf-8")
 
 app.get('/', function(request, response) {
-  response.send(buffer);
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
